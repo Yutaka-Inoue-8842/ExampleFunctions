9877d952c08955eaa2f65de2dfec7facb5d05c88
YutakaInoue <inoue.8842@gmail.com>
2022-09-28 11:27:35 +0900

    Initial Commit

---
+++ ExampleFunctions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleFunctions
+//
+//  Created by Yutaka Inoue on 2022/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
